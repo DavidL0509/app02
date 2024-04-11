e24b67ebb5d3be7c24ba0b4babc3b6d3eb933fbd
DavidL0509 <152031021+DavidL0509@users.noreply.github.com>
2024-04-11 18:28:45 +0200

    Add kommuneLayerCheckbox

---
+++ src/modules/kommune/kommuneLayerCheckbox.tsx
@@ -0,0 +1,40 @@
+import React, { useContext, useEffect, useState } from "react";
+import { MapContext } from "../map/mapContext";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import { GeoJSON } from "ol/format";
+
+const kommuneLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/app02/kommuner.json",
+    format: new GeoJSON(),
+  }),
+});
+
+export function KommuneLayerCheckbox() {
+  const [checked, setChecked] = useState(true);
+
+  const { setLayers } = useContext(MapContext);
+
+  useEffect(() => {
+    if (checked) {
+      setLayers((old) => [...old, kommuneLayer]);
+    }
+    return () => {
+      setLayers((old) => old.filter((l) => l !== kommuneLayer));
+    };
+  }, [checked]);
+
+  return (
+    <div>
+      <label>
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+        />
+        {checked ? "Hide" : "Show"} kommuner
+      </label>
+    </div>
+  );
+}
